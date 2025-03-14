d123bd4a894c7845a48db166de5d092c9218645e
noz-23 <nzds23@yahoo.co.jp>
2025-03-14 15:39:13 +0900

    0.1.0 整理中

---
+++ Controls/AnalysisControl.xaml.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Win32;
+using RepriseReportLogAnalyzer.Files;
+using RepriseReportLogAnalyzer.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RepriseReportLogAnalyzer.Controls
+{
+    /// <summary>
+    /// AnalysisControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AnalysisControl : UserControl
+    {
+        public AnalysisControl()
+        {
+            InitializeComponent();
+
+            AnalysisManager.Instance.SetProgressCount(_progressCount);
+        }
+        private const string _ANALYSIS = "[File Read]";
+
+        private string _resultTitle =string.Empty;
+
+        private void _openClick(object sender_, RoutedEventArgs e_)
+        {
+            var dlg = new OpenFileDialog()
+            {
+                Title = "Please Select Reprise Report Log Files",
+                Filter = "Reprise Report Log|*.*",
+                Multiselect = true
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                var list = dlg.FileNames.ToList();
+                list.Sort();
+                list.ForEach(path_ => _dataGrid.Items.Add(path_));
+            }
+        }
+
+        private void _selectClick(object sender_, RoutedEventArgs e_)
+        {
+            var dlg = new OpenFolderDialog()
+            {
+                Title = "Please Select Output Folder",
+                Multiselect = false
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                _textBoxFolder.Text = dlg.FolderName;
+            }
+        }
+
+        private async void _analysisClick(object sender_, RoutedEventArgs e_)
+        {
+            _buttonAnalysis.IsEnabled = false;
+            var outFolder = _textBoxFolder.Text;
+            await Task.Run(() =>
+            {
+                int count = 0;
+                int max = _dataGrid.Items.Count;
+                _progressCount(0, max, _ANALYSIS);
+
+                var analysis = new AnalysisReportLog();
+                foreach (string path_ in _dataGrid.Items)
+                {
+                    LogFile.Instance.WriteLine($"LogAnalysis: {path_}");
+                    analysis.StartAnalysis(path_);
+                    _progressCount(++count, max);
+                }
+                analysis.EndAnalysis();
+                //_calendarShow(analysis.ListDate);
+
+                AnalysisManager.Instance.Analysis(analysis);
+            });
+
+            _buttonAnalysis.IsEnabled = true;
+        }
+
+
+        private void _deleteClick(object sender_, RoutedEventArgs e_)
+        {
+            var select = _dataGrid.SelectedItem;
+
+            _dataGrid.Items.Remove(select);
+        }
+
+        private void _progressCount(int count_, int max_, string str_ = "")
+        {
+            if (count_ == 0)
+            {
+                App.Current.Dispatcher.Invoke(() =>
+                {
+                    _resultTitle = str_;
+                    _progressBar.Maximum = max_;
+                });
+            }
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                _progressBar.Value = count_;
+                _textBlock.Text = $"{count_} / {max_} {_resultTitle}".Trim();
+            });
+        }
+    }
+}
---
+++ Controls/ResultControl.xaml.cs
@@ -0,0 +1,58 @@
+﻿using RepriseReportLogAnalyzer.Files;
+using RepriseReportLogAnalyzer.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RepriseReportLogAnalyzer.Controls
+{
+    /// <summary>
+    /// ResultControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ResultControl : UserControl
+    {
+        public ResultControl()
+        {
+            InitializeComponent();
+        }
+
+
+        private void _calendarSelected(object sender_, System.Windows.Controls.SelectionChangedEventArgs e_)
+        {
+            //if( sender_ is Calendar calendar )
+            //{
+                LogFile.Instance.WriteLine($"Selected {_calendar.SelectedDate}");
+                AnalysisManager.Instance.SetDate(_calendar.SelectedDate);
+            //}
+        }
+
+        public void CalendarShow(IEnumerable<DateTime> list_)
+        {
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                _calendar.DisplayDateStart = list_.First();
+                _calendar.DisplayDateEnd = list_.Last();
+
+                //_calendar.SelectedDates.Clear();
+                //list_.ToList().ForEach(date_ => _calendar.SelectedDates.Add(date_));
+            });
+
+        }
+
+        private void _mouseDoubleClick(object sender_, MouseButtonEventArgs e_)
+        {
+            _calendar.SelectedDate = null;
+        }
+    }
+}
